2665cdd1d0bf94a723118a274971f76490743152
Gabriel Carvalho <cttbiel@gmail.com>
2024-07-31 19:25:16 -0300

    teste

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,7 +3,7 @@ import { songsData, assets } from '../assets/assets'
 
 const Player = () => {
   return (
-    <div className='h-[10%] bg-black flex justify-between items-center text-white px-4'>
+      <div className='h-[10%] bg-black flex justify-between items-center text-white px-4'>
         <div className='hidden lg:flex items-center gap-4'>
             <img className='w-12' src={songsData[0].image} alt="" />
             <div>
@@ -23,13 +23,25 @@ const Player = () => {
         <div className='flex items-center gap-5'>
           <p>1:06</p> {/*Tempo inicial*/}
           <div className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'> 
-            <hr className='h-1 border-none w-0 bg-green-800 rounded-full' /> {/*Barrinha de progressão do vídeo*/}
+            <hr className='h-1 border-none w-0 bg-green-800 rounded-full' /> {/*Barrinha de progressão do vídeo, dá pra mudar o quanto avança no "w-n"*/}
           </div>
           <p>3:20</p>{/*Tempo final da música*/}
 
         </div>
 
-    </div>
+      </div>
+      <div className='hidden lg:flex items-center gap-2 opacity-75'>
+        <img className='w-4' src={assets.plays_icon} alt="" />
+        <img className='w-4' src={assets.mic_icon} alt="" />
+        <img className='w-4' src={assets.queue_icon} alt="" />
+        <img className='w-4' src={assets.speaker_icon} alt="" />
+        <img className='w-4' src={assets.volume_icon} alt="" />
+        <div className='w-20 bg-slate-50 h-1 rounded'> {/*Div para progressão do volume*/}
+
+        </div>
+        <img className='w-4' src={assets.mini_player_icon} alt="" />
+        <img className='w-4' src={assets.zoom_icon} alt="" />
+      </div>
     </div>
   )
 }
