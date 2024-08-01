114cecc1e773375ed1b086fa309dc81b20f550ea
Gabriel Carvalho <cttbiel@gmail.com>
2024-07-31 21:11:56 -0300

    Começando o álbum

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 import Navbar from './Navbar'
 import { albumsData } from '../assets/assets'
 import AlbumItems from './AlbumItems'
+import { songsData } from '../assets/assets'
+import SongItem from './SongItem'
 
 const DisplayHome = () => {
   return (
     <>
     <Navbar />
     <div className='mb-4'>
-        {albumsData.map((item,index)=>(<AlbumItems key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/> ))}
+        <h1 className='my-5 font-bold text-2xl'>Destaques da Semana</h1>
+        <div className='flex overflow-auto'>
+            {albumsData.map((item,index)=>(<AlbumItems key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} /> ))}
+        </div>        
+    </div>
+    <div className='mb-4'>
+        <h1 className='my-5 font-bold text-2xl'>Destaques de hoje</h1>
+        <div className='flex overflow-auto'>
+            {songsData.map((item,index)=>(<SongItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+        </div>        
     </div>
     </>
   )
