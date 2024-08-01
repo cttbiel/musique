0e5e4d0a7c04f451ef6ef4327922d9991746aaee
Gabriel Carvalho <cttbiel@gmail.com>
2024-07-31 23:53:59 -0300

    tempo quais cabano

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useRef, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 import { songsData } from "../assets/assets";
 
 export const PlayerContext = createContext();
@@ -32,6 +32,30 @@ const PlayerContextProvider = (props) => {
         setPlayStatus(false);
     } 
 
+    const playWithId = async (id) => {
+        await setTrack(songsData[id]);
+        await audioRef.current.play();
+        setPlayStatus(true)
+    }
+
+    useEffect (() =>{
+        setTimeout(()=> {
+            audioRef.current.ontimeupdate = () => {
+                seekBar.current.style.width = (Math.floor(audioRef.current.currentTime/audioRef.current.duration*100))+"%";
+                setTime({
+                    currentTime: {
+                        second: Math.floor(audioRef.current.currentTime % 60),
+                        minute: Math.floor(audioRef.current.currentTime / 60)
+                    },
+                    totalTime: {
+                        second: Math.floor(audioRef.current.duration % 60),
+                        minute: Math.floor(audioRef.current.duration / 60)
+                    }
+                })
+            }
+        },1000);
+    },[audioRef])
+
     const contextValue = {
         audioRef,
         seekBar,
@@ -39,7 +63,8 @@ const PlayerContextProvider = (props) => {
         track,setTrack,
         playStatus,setPlayStatus,
         time,setTime,
-        play,pause
+        play,pause,
+        playWithId
 
     }
 
